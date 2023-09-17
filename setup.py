099c2357c324836c9dc00385da62621b6fbcc5bb
zetro <83398581+zetrobt@users.noreply.github.com>
2023-09-17 11:12:47 +0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Dreading",
+    version="1.0",
+    description="Decorator-like Threads for Python",
+    url="https://github.com/zetrobt/dreading",
+    download_url="https://github.com/zetrobt/dreading/releases/latest",
+    author="zetro.",
+    author_email="me@zetro.ga",
+    license="MIT",
+    python_requires="~=3.7",
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[]
+)
